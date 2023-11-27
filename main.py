9115d178c93db5f1edbfc8b0709a794f36950f4c
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-27 19:59:44 +0200

    class LinkedList

--- main.py
+++ main.py
@@ -7,12 +7,25 @@ class Node:
     def __str__(self):
         return f"[{self.data}] -> {self.next}"
 
-node = Node(1)
-current_node = node
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-for i in range(2, 360):
-    new_node = Node(i)
-    current_node.next = new_node
-    current_node = new_node
+    def __str__(self):
+        return f"{self.head}"
 
-print(node)
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node #новий елемент стає початком
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next #переміщуємося до наступного елементу
+        last_node.next = new_node
+my_lst = LinkedList()
+my_lst.append(1)
+my_lst.append(2)
+my_lst.append(3)
+my_lst.append(4)
+print(my_lst)
