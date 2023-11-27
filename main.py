e2609ab85149769a131cb36c7e1e5f66a2448995
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-27 21:07:48 +0200

    DoublyLinkedList

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
-#Синтаксис Однозв'язковий список
+#Синтаксис Двозв'язаний список
 class Node:
     def __init__(self, data):
         self.data = data
+        self.prev = None
         self.next = None
 
     def __str__(self):
-        return f"[{self.data}] -> {self.next}"
+        return f"{self.prev} <- [{self.data}] -> {self.next} "
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
-
-    def __str__(self):
-        return f"{self.head}"
 
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
-            self.head = new_node #новий елемент стає початком
+            self.head = new_node
             return
         last_node = self.head
         while last_node.next:
-            last_node = last_node.next #переміщуємося до наступного елементу
+            last_node = last_node.next
         last_node.next = new_node
-my_lst = LinkedList()
-my_lst.append(1)
-my_lst.append(2)
-my_lst.append(3)
-my_lst.append(4)
-print(my_lst)
+        new_node.prev = last_node
+
+my_list = DoublyLinkedList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+my_list.append(10)
+#додавання елементу на початок списку
+new_node = Node(0)
+new_node.next = my_list.head
+if my_list.head is not None:
+    my_list.head.prev = new_node
+my_list.head = new_node
+#додавання елементу на другу позицію списку
+new_node = Node(4)
+current = my_list.head
+while current.data != 5:
+    current = current.next
+if current.next is not None:
+    current.next.prev = new_node
+current.next = new_node
+new_node.prev = current
+#виведення списку
+current = my_list.head
+elements = []
+while current:
+    elements.append(str(current.data))
+    current = current.next
+print(" <-> ".join(elements))
